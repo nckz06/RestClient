13a3a6a5aac9769047129671a4bc2a9e9d6f1d65
Nicolas Nogueira <nsn.nicolas@gmail.com>
2026-06-01 10:21:18 -0300

    Inicialização da API Rest Client

---
+++ src/test/java/br/gm/nicolas/RestClient/RestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gm.nicolas.RestClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
